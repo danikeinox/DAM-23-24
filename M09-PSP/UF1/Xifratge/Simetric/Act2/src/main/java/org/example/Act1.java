7ffa2ae88834855f80b9191a7b76614cc11ed5bd
Daniel Cabrera <enamber01@gmail.com>
2023-10-26 21:48:32 +0200

    Added M09-PSP Xifratge Simetric Act2 data

--- M09-PSP/UF1/Xifratge/Simetric/Act2/src/main/java/org/example/Act1.java
+++ M09-PSP/UF1/Xifratge/Simetric/Act2/src/main/java/org/example/Act1.java
@@ -1,2 +1,21 @@
-package org.example;public class Act1 {
+package org.example;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class Act1 {
+    public static void act1() throws Exception {
+        String clau = "clau_secreta_aes";  // Asegura't que la clau és de 16 caràcters (128 bits)
+        SecretKeySpec secretKey = new SecretKeySpec(clau.getBytes(), "AES");
+
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] textXifrat = cipher.doFinal("Text secret".getBytes());
+        System.out.println("Text xifrat: " + Base64.getEncoder().encodeToString(textXifrat));
+
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] textDesxifrat = cipher.doFinal(textXifrat);
+        System.out.println("Text desxifrat: " + new String(textDesxifrat));
+    }
 }
--- M09-PSP/UF1/Xifratge/Simetric/Act2/src/main/java/org/example/Main.java
+++ M09-PSP/UF1/Xifratge/Simetric/Act2/src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            menu();
+            Scanner sc = new Scanner(System.in);
+            int option;
+            System.out.print("Selecciona una opció: ");
+            option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    Act1.act1();
+                    break;
+                case 2:
+                    //Act2.act2();
+                    break;
+                case 3:
+                    //Act3.act3();
+                    break;
+                case 4:
+                    //Act4.act4();
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opcio incorrecta");
+                    break;
+            }
+
+        }
+    }
+
+    private static void menu() {
+        System.out.println("+------------------------------------+\n" +
+                "|          e·ni-gm·a system          |\n" +
+                "+------------------------------------+\n" +
+                "| 1. Encriptar fitxer                |\n" +
+                "| 2. Desencriptar fitxer             |\n" +
+                "| 3. Generar un hash d'un missatge   |\n" +
+                "| 4. Verificar el hash d'un missatge |\n" +
+                "| 5. Sortir                          |\n" +
+                "+------------------------------------+\n");
     }
 }

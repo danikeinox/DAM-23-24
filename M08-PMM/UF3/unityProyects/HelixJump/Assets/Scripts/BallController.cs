323c03946a153b9aa8469aa367628a8e993ed641
Daniel Cabrera <enamber01@gmail.com>
2024-03-11 15:12:38 +0100

    ADD M08-PMM UF3 HelixJump UnityProjects

---
+++ M08-PMM/UF3/unityProyects/HelixJump/Assets/Scripts/BallController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float impulseForce = 5f;
+    public GameObject splitPrefab;
+    private Vector3 startPos;
+    public int perfectPass = 0;
+    private bool ignoreNextCollision;
+    public bool isSuperSpeedActive;
+    private List<GameObject> splits = new List<GameObject>();
+
+    private void Awake()
+    {
+        startPos = transform.position;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (ignoreNextCollision)
+            return;
+        if (isSuperSpeedActive)
+        {
+            if (!other.transform.GetComponent<Goal>())
+            {
+                Destroy(other.transform.parent.gameObject);
+            }
+        }
+        else
+        {
+            DeathPart deathPart = other.transform.GetComponent<DeathPart>();
+            if (deathPart)
+            {
+                deathPart.HittedDeathPart();
+            }
+        }
+
+        rb.velocity = Vector3.zero;
+        rb.AddForce(Vector3.up * impulseForce, ForceMode.Impulse);
+
+        GameObject newSplit = Instantiate(splitPrefab, new Vector3(transform.position.x, other.transform.position.y + 0.36f, transform.position.z), transform.rotation);
+        newSplit.transform.localScale = Vector3.one * Random.Range(0.8f, 1.2f);
+        newSplit.transform.parent = other.transform;
+
+        splits.Add(newSplit);
+
+        ignoreNextCollision = true;
+        Invoke("AllowCollision", .2f);
+
+        perfectPass = 0;
+        isSuperSpeedActive = false;
+    }
+
+    public void ClearSplits()
+    {
+        foreach (GameObject split in splits)
+        {
+            Destroy(split);
+        }
+        splits.Clear();
+    }
+
+    private void Update()
+    {
+        if (perfectPass >= 3 && !isSuperSpeedActive)
+        {
+            isSuperSpeedActive = true;
+            rb.AddForce(Vector3.down * 10, ForceMode.Impulse);
+        }
+    }
+
+    public void ResetBall()
+    {
+        transform.position = startPos;
+        ClearSplits();
+    }
+
+    private void AllowCollision()
+    {
+        ignoreNextCollision = false;
+    }
+
+    public void SetSplitColor(Color color)
+    {
+        splitPrefab.GetComponent<Renderer>().sharedMaterial.color = color;
+    }
+}
---
+++ M08-PMM/UF3/unityProyects/HelixJump/Assets/Scripts/GameManager.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager singleton;
+    public int best;
+    public int score;
+    public int currentStage = 0;
+    public static int passCheckRing;
+
+    public static bool gameOver;
+    public static bool levelWin;
+
+    public GameObject GamePanel;
+    public GameObject GameOverPanel;
+    public GameObject levelWinPanel;
+
+    public TextMeshProUGUI currentLevelText;
+    public TextMeshProUGUI nextLevelText;
+    public TextMeshProUGUI currentScoreText;
+    public TextMeshProUGUI bestScoreText;
+
+    public Slider ProgressBar;
+
+
+    private void Awake()
+    {
+        if (singleton == null)
+            singleton = this;
+        else if (singleton != this)
+            Destroy(gameObject);
+
+        // Load the saved highscore
+        best = PlayerPrefs.GetInt("Highscore");
+        currentStage = PlayerPrefs.GetInt("currentStage", 1);
+    }
+
+    private void Start()
+    {
+        passCheckRing = 0;
+    }
+
+    public void Update()
+    {
+        if (!gameOver && !levelWin)
+        {
+            GamePanel.SetActive(true);
+            GameOverPanel.SetActive(false);
+            levelWinPanel.SetActive(false);
+            bestScoreText.text = "Best: " + GameManager.singleton.best;
+            currentScoreText.text = "Score: " + GameManager.singleton.score;
+            if (currentStage <= FindObjectOfType<HelixController>().levelsCount)
+            {
+                currentLevelText.text = currentStage.ToString();
+                nextLevelText.text = (currentStage + 1).ToString();
+            }
+            else
+            {
+                currentLevelText.text = currentStage.ToString();
+                nextLevelText.text = currentStage.ToString();
+            }
+            int progress = passCheckRing * 100 / FindObjectOfType<HelixController>().levelsCount;
+            ProgressBar.value = progress;
+        }
+
+        if (gameOver)
+        {
+            if (levelWin)
+            {
+                levelWinPanel.SetActive(false);
+            }
+            GamePanel.SetActive(false);
+            GameOverPanel.SetActive(true);
+            if (Input.GetMouseButtonDown(0))
+            {
+                PlayerPrefs.SetInt("CurrentLevelIndex", currentStage + 1);
+                SceneManager.LoadScene(0);
+                gameOver = false;
+                singleton.score = 0;
+                FindObjectOfType<BallController>().ResetBall();
+                FindObjectOfType<HelixController>().LoadStage(0);
+            }
+        }
+
+        if (levelWin)
+        {
+            if (gameOver)
+            {
+                levelWin = false;
+                gameOver = true;
+            }
+            else
+            {
+                GamePanel.SetActive(false);
+                levelWinPanel.SetActive(true);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    NextLevel();
+                }
+            }
+        }
+    }
+
+    public void NextLevel()
+    {
+        currentStage++;
+        levelWin = false;
+        FindObjectOfType<BallController>().ResetBall();
+        FindObjectOfType<HelixController>().LoadStage(currentStage);
+        Debug.Log("Next Level");
+        passCheckRing = 0;
+    }
+
+    public void WinLevel()
+    {
+        Debug.Log("Win Level");
+        levelWin = true;
+    }
+
+    public void RestartLevel()
+    {
+        Debug.Log("Restarting Level");
+        gameOver = true;
+    }
+
+    public void AddScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+
+        if (score > best)
+        {
+            PlayerPrefs.SetInt("Highscore", score);
+            best = score;
+        }
+    }
+
+
+}
